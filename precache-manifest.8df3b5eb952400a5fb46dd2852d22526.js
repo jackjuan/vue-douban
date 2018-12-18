9c606726805f5bda79a036eb2339290f0829142c
JUAN1996 <1352787908@qq.com>
2018-12-18 16:46:32 +0800

    vue-douban

---
+++ precache-manifest.8df3b5eb952400a5fb46dd2852d22526.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2894918c2d3fad3f8337",
+    "url": "/js/chunk-vendors.8e475858.js"
+  },
+  {
+    "revision": "6601a3d10018c3199bc9",
+    "url": "/js/chunk-21c35d62.2248e762.js"
+  },
+  {
+    "revision": "596993acb2f6aeffd824",
+    "url": "/js/app.662a08f4.js"
+  },
+  {
+    "revision": "74bcc0b3881ca9af3f267260f82af41b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6601a3d10018c3199bc9",
+    "url": "/css/chunk-21c35d62.b4e134b7.css"
+  },
+  {
+    "revision": "596993acb2f6aeffd824",
+    "url": "/css/app.3eb8e688.css"
+  }
+];
